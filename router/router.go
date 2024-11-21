c1c19d63b4a48e74e209d973a1ac75a0fd50ad8f
nurmuh-alhakim18 <alhakim2318@gmail.com>
2024-11-21 07:34:23 +0700

    add check health

--- router/router.go
+++ router/router.go
@@ -4,10 +4,15 @@ import (
 	"net/http"
 
 	"github.com/nurmuh-alhakim18/url-shortener-api/internal/handlers/url"
+	"github.com/nurmuh-alhakim18/url-shortener-api/pkg/utils"
 )
 
 func NewRouter(urlHandler *url.URLHandler) *http.ServeMux {
 	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
+		utils.Response(w, http.StatusOK, "OK")
+	})
 
 	mux.HandleFunc("POST /api/shorten", urlHandler.HandlerShortenURL)
 	mux.HandleFunc("GET /{alias}", urlHandler.HandlerRedirectURL)
